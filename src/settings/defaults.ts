e1fec807f33c2184c0bffb86068b094303b3f483
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-03-14 20:29:31 -0300

    prepare stable version

--- src/settings/defaults.ts
+++ src/settings/defaults.ts
@@ -1,7 +1,7 @@
 import { ConsentStatus } from '@splitsoftware/splitio-commons/src/types';
 import { CONSENT_GRANTED } from '@splitsoftware/splitio-commons/src/utils/constants';
 
-const packageVersion = '0.6.2-rc.0';
+const packageVersion = '0.6.2';
 
 export const defaults = {
   startup: {
