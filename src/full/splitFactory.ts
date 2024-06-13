2c75eb8f7e836028a5c85deef68c615e994cd8bb
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-06-13 20:58:57 -0300

    Upgrade JS-commons dependency

--- src/full/splitFactory.ts
+++ src/full/splitFactory.ts
@@ -2,6 +2,7 @@ import { settingsFactory } from '../settings/full';
 import { getModules } from '../platform/getModules';
 import { sdkFactory } from '@splitsoftware/splitio-commons/src/sdkFactory/index';
 import type { ISdkFactoryParams } from '@splitsoftware/splitio-commons/src/sdkFactory/types';
+import { IReactNativeSettings } from '../../types/splitio';
 
 /**
  * SplitFactory for React Native.
@@ -12,7 +13,7 @@ import type { ISdkFactoryParams } from '@splitsoftware/splitio-commons/src/sdkFa
  * caution since, unlike `config`, this param is not validated neither considered part of the public API.
  * @throws Will throw an error if the provided config is invalid.
  */
-export function SplitFactory(config: any, __updateModules?: (modules: ISdkFactoryParams) => void) {
+export function SplitFactory(config: IReactNativeSettings, __updateModules?: (modules: ISdkFactoryParams) => void) {
   const settings = settingsFactory(config);
   const modules = getModules(settings);
   if (__updateModules) __updateModules(modules);
