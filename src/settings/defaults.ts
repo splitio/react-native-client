fd626dc8a0d5c9a70bb2699a711d2c622a2c5c8e
Emiliano Sanchez <emiliano.sanchez@split.io>
2024-11-11 10:59:48 -0300

    rc

--- src/settings/defaults.ts
+++ src/settings/defaults.ts
@@ -1,7 +1,7 @@
 import type SplitIO from '@splitsoftware/splitio-commons/types/splitio';
 import { CONSENT_GRANTED } from '@splitsoftware/splitio-commons/src/utils/constants';
 
-const packageVersion = '1.0.1';
+const packageVersion = '1.0.1-rc.0';
 
 export const defaults = {
   startup: {
