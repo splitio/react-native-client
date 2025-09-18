5098034305acd9227d7993090ab6e96d2da82e79
Emiliano Sanchez <emiliano.sanchez@split.io>
2025-09-18 12:06:57 -0300

    Upgrade JS-commons

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,42 +11,42 @@ declare module JsSdk {
    * Split.io SDK factory function.
    *
    * The settings parameter should be an object that complies with the SplitIO.IReactNativeSettings.
-   * For more information read the corresponding article: @see {@link https://help.split.io/hc/en-us/articles/4406066357901#configuration}
+   * For more information read the corresponding article: @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/react-native-sdk/#configuration}
    */
   export function SplitFactory(settings: SplitIO.IReactNativeSettings): SplitIO.IBrowserSDK;
 
   /**
-   * Persistent storage. By default, it uses the browser's LocalStorage API.
+   * Persistent storage. By default, it uses the browser's LocalStorage API if available.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/4406066357901#configuring-localstorage-cache-for-the-sdk}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/react-native-sdk/#configuring-persistent-cache-for-the-sdk}
    */
   export function InLocalStorage(options?: SplitIO.InLocalStorageOptions): SplitIO.StorageSyncFactory;
 
   /**
    * Creates a logger instance that enables descriptive log messages with DEBUG log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/4406066357901#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/react-native-sdk/#logging}
    */
   export function DebugLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with INFO log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/4406066357901#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/react-native-sdk/#logging}
    */
   export function InfoLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with WARN log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/4406066357901#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/react-native-sdk/#logging}
    */
   export function WarnLogger(): SplitIO.ILogger;
 
   /**
    * Creates a logger instance that enables descriptive log messages with ERROR log level when passed in the factory settings.
    *
-   * @see {@link https://help.split.io/hc/en-us/articles/4406066357901#logging}
+   * @see {@link https://developer.harness.io/docs/feature-management-experimentation/sdks-and-infrastructure/client-side-sdks/react-native-sdk/#logging}
    */
   export function ErrorLogger(): SplitIO.ILogger;
 }
