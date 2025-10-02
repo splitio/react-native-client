bbf19b186350ca3f1062c382515c1d1d32b08ff7
Emiliano Sanchez <emiliano.sanchez@split.io>
2025-10-02 14:06:16 -0300

    feat: add EventSource polyfill for improved streaming compatibility

--- src/platform/getEventSource.ts
+++ src/platform/getEventSource.ts
@@ -1,4 +1,6 @@
 import reactNative from 'react-native';
+// @ts-expect-error no declaration file provided
+import { EventSourceXHR } from './EventSourceXHR/EventSourceXHR';
 
 let _RNEventSource: typeof EventSource | undefined;
 
@@ -8,9 +10,15 @@ try {
 } catch (e) {}
 
 /**
- * Returns native implementation of EventSource. If not available (e.g., Expo or other runtime than Android and iOS),
- * checks if global EventSource is available and returns it.
+ * Returns native implementation of EventSource.
+ * If not available (e.g., incomplete linking, Expo projects or other runtimes than Android and iOS), checks if global EventSource is available and returns it.
+ * If not available, returns an EventSource polyfill based on XMLHttpRequest.
+ *
+ * The EventSource polyfill is the last option because it doesn't work on Android in debug mode in RN below v0.74,
+ * due to Flipper network interceptor (https://github.com/NepeinAV/rn-eventsource-reborn#eventsource-dont-works-on-android-in-debug-mode).
+ * In RN 0.74 (https://reactnative.dev/blog/2024/04/22/release-0.74#removal-of-flipper-react-native-plugin) and Expo 51 (https://github.com/expo/expo/issues/27526#issuecomment-2113893318),
+ * Flipper was removed from new app templates and replaced by the new React Native DevTools, so the Android-debug interception that broke SSE disappears.
  */
 export function getEventSource(): typeof EventSource | undefined {
-  return _RNEventSource || (typeof EventSource === 'function' ? EventSource : undefined);
+  return _RNEventSource || (typeof EventSource === 'function' ? EventSource : EventSourceXHR);
 }
